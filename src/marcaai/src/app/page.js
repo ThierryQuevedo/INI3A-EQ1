9b310f1e18bba4e8e2e5f84230e028e23d80ca73
Thierry <thierry.quevedo@unesp.br>
2026-03-29 21:55:57 -0300

    ademas

--- src/marcaai/src/app/page.js
+++ src/marcaai/src/app/page.js
@@ -1,5 +1,4 @@
-import { discoverValidationDepths } from "next/dist/server/app-render/instant-validation/instant-validation";
-import Header from "@/components/Header";
+import Header from "../components/Header";
 
 export default function Home() {
   return (
--- src/marcaai/src/components/Header.jsx
+++ src/marcaai/src/components/Header.jsx
@@ -4,8 +4,8 @@ export default function Header(){
     return(
         <header className="bg-blue-300 flex items-center justify-between px-10 h-15">
             <h1 className="text-2xl">Marca Aí</h1>
-            <a href="" className=''>
-                <User className='justify-center bg-green-700 rounded-full '/>    
+            <a href="" className='bg-green-700 rounded-full p-2 hover:bg-red-500 hover:scale-105 transition-all duration-200 '>
+                <User className='justify-center '/>    
             </a>
 
         </header>
